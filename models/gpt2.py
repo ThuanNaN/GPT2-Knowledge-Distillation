a7712b0f50ae439d8913d28ef8dc0ab91931833e
ThuanNaN <duongthuan1445@gmail.com>
2023-07-16 03:14:39 +0700

    update: logger

--- models/gpt2.py
+++ models/gpt2.py
@@ -5,9 +5,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 from models import GPT2Config
 from optimizers import SophiaG
-from utils.logger import get_logger
-
-LOGGER = get_logger("GPT2-model")
+from utils import logger
 
 optimizer_dict = {'adamw': torch.optim.AdamW,
                   'sophiag': SophiaG
@@ -51,7 +49,7 @@ class CausalSelfAttention(nn.Module):
         # flash attention (only support in Pytorch >0 2.0)
         # self.flash = hasattr(F, 'scaled_dot_product_attention')
         # if not self.flash:
-        #     LOGGER.info("WARNING: using slow attention. Flash Attention requires Pytorch >= 2.0")
+        #     logger.info("WARNING: using slow attention. Flash Attention requires Pytorch >= 2.0")
         #     # causal mask to ensure that attention is only applied to the left in the input sequence
         #     self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
         #                          ).view(1,1,config.block_size, config.block_size)
@@ -197,13 +195,13 @@ class GPT2(nn.Module):
             'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
             'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
         }[model_type]
-        LOGGER.info("forcing vocab_size=50257, block_size=1024, bias=True")
+        logger.info("forcing vocab_size=50257, block_size=1024, bias=True")
         config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
         config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
         config_args['bias'] = True # always True for GPT model checkpoints
         # we can override the dropout rate, if desired
         if 'dropout' in override_args:
-            LOGGER.info(f"overriding dropout rate to {override_args['dropout']}")
+            logger.info(f"overriding dropout rate to {override_args['dropout']}")
             config_args['dropout'] = override_args['dropout']
         # create a from-scratch initialized minGPT model
         config = GPT2Config(**config_args)
@@ -292,7 +290,7 @@ class GPT2(nn.Module):
         if optimizer_name == 'adamw':
         # new PyTorch nightly has a new 'fused' option for AdamW that is much faster
             use_fused = (device_type == 'cuda') and ('fused' in inspect.signature(torch.optim.AdamW).parameters)
-            LOGGER.info(f"using fused AdamW: {use_fused}")
+            logger.info(f"using fused AdamW: {use_fused}")
             extra_args = dict(fused=True) if use_fused else dict()
             optimizer = opt_func(optim_groups, lr=learning_rate, betas=betas, **extra_args)
         elif optimizer_name == 'sophiag':
