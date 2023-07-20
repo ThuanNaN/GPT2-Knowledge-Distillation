f36a08bfe31d0c32bc0edd701ac191573e185aeb
ThuanNaN <duongthuan1445@gmail.com>
2023-07-20 23:26:43 +0700

    update: train cfg

--- config/finetune_gpt2m.py
+++ config/finetune_gpt2m.py
@@ -1,25 +1,25 @@
-import time
+wandb_log = False 
+wandb_project = 'mle'
+wandb_run_name = 'ft-teacher'
 
-out_dir = 'out-shakespeare'
-eval_interval = 5
-eval_iters = 40
-wandb_log = False # feel free to turn on
-wandb_project = 'shakespeare'
-wandb_run_name = 'ft-' + str(time.time())
+out_dir = 'out-shakespeare-char-gpt2m'
 
-dataset = 'shakespeare'
-init_from = 'gpt2-medium' # this is the largest GPT-2 model
+dataset = 'shakespeare_char'
 
-# only save checkpoints if the validation loss improves
+log_interval = 10 
+
+eval_interval = 10
+eval_iters = 200
 always_save_checkpoint = False
 
-# the number of examples per iter:
-# 1 batch_size * 32 grad_accum * 1024 tokens = 32,768 tokens/iter
-# shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
-batch_size = 1
-gradient_accumulation_steps = 32
-max_iters = 20
+init_from = 'gpt2-medium'
 
-# finetune at constant LR
-learning_rate = 3e-5
+
+batch_size = 4
+gradient_accumulation_steps = 16
+
+block_size = 1024 
+
+max_iters = 500
+learning_rate = 2e-5
 decay_lr = False
