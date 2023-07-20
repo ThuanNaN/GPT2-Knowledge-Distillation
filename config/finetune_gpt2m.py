1b07d7f74506fa43d1c2092a75aa1decb54f1418
ThuanNaN <duongthuan1445@gmail.com>
2023-07-21 01:08:21 +0700

    update: train script

--- config/finetune_gpt2m.py
+++ config/finetune_gpt2m.py
@@ -1,4 +1,4 @@
-wandb_log = False 
+wandb_log = True 
 wandb_project = 'mle'
 wandb_run_name = 'ft-teacher'
 
@@ -8,18 +8,17 @@ dataset = 'shakespeare_char'
 
 log_interval = 10 
 
-eval_interval = 20
+eval_interval = 50
 eval_iters = 500
 always_save_checkpoint = False
 
 init_from = 'gpt2-medium'
-
+dropout = 0.01
 
 batch_size = 4
 gradient_accumulation_steps = 16
 
-block_size = 1024 
 
 max_iters = 500
-learning_rate = 2e-5
+learning_rate = 5e-5
 decay_lr = False
