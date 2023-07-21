e21bfa3516a656242bf1d3b85e0539ca32c8c066
ThuanNaN <duongthuan1445@gmail.com>
2023-07-21 17:44:10 +0700

    update: teacher train cfg

--- config/finetune_gpt2m.py
+++ config/finetune_gpt2m.py
@@ -1,6 +1,6 @@
 wandb_log = True 
 wandb_project = 'mle'
-wandb_run_name = 'ft-teacher'
+wandb_run_name = 'ft-teacher-adamw-drop01'
 
 out_dir = 'out-shakespeare-char-gpt2m'
 
@@ -13,12 +13,12 @@ eval_iters = 500
 always_save_checkpoint = False
 
 init_from = 'gpt2-medium'
-dropout = 0.01
+dropout = 0.1
 
 batch_size = 4
 gradient_accumulation_steps = 16
 
 
 max_iters = 500
-learning_rate = 5e-5
+learning_rate = 1e-4
 decay_lr = False
