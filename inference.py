8ba4b7e117340e9629da574f6a44c6aa88c83597
ThuanNaN <duongthuan1445@gmail.com>
2023-07-30 20:37:58 +0700

    update: load model

--- inference.py
+++ inference.py
@@ -15,31 +15,21 @@ num_samples = 5
 max_new_tokens = 200
 device = 'cuda' 
 
+
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
 device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
 ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
-ckpt_path = './ckpt/teacher.pt'
+ckpt_path = './ckpt/student.pt'
 model = model_from_ckpt(ckpt_path, device)
 model.to(device)
 
-load_meta = True
-meta_path = "./data/shakespeare_char/meta.pkl"
-if load_meta:
-    print(f"Loading meta from {meta_path}...")
-    with open(meta_path, 'rb') as f:
-        meta = pickle.load(f)
-    # TODO want to make this more general to arbitrary encoder/decoder schemes
-    stoi, itos = meta['stoi'], meta['itos']
-    encode = lambda s: [stoi[c] for c in s]
-    decode = lambda l: ''.join([itos[i] for i in l])
-else:
-    # ok let's assume gpt-2 encodings by default
-    print("No meta.pkl found, assuming GPT-2 encodings...")
-    enc = tiktoken.get_encoding("gpt2")
-    encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
-    decode = lambda l: enc.decode(l)
+# ok let's assume gpt-2 encodings by default
+print("No meta.pkl found, assuming GPT-2 encodings...")
+enc = tiktoken.get_encoding("gpt2")
+encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
+decode = lambda l: enc.decode(l)
 
 
 start = "What is the answer to work?"
@@ -49,7 +39,7 @@ x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 # run generation
 with torch.no_grad():
     with ctx:
-        for k in range(num_samples):
+        for k in range(1):
             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
             print(decode(y[0].tolist()))
             print('---------------')
---
+++ run/train_student_distill.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+python train_adamw.py --wandb-log \
+                        --save-dir shakespeare-char-baby \
+                        --dataset shakespeare_char \
+                        --log-interval 10 \
+                        --eval-interval 200 \
+                        --eval-iters 500 \
+                        --batch-size 64 \
+                        --accumulation-steps 1 \
+                        --block-size 256 \
+                        --num-layer 6 \
+                        --num-head 6 \
+                        --num-embd 384 \
+                        --dropout 0.2 \
+                        --max-iters 5000 \
+                        --learning-rate 1e-3 \
+                        --decay-lr \
+                        --lr-decay-iters 5000 \
+                        --min-lr 1e-4 \
+                        --beta2 0.99 \
+                        --warmup-iters 200 
